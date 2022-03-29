8719477f947dd80c9e86ae4fdec0773c59065c12
Sanzio <sanzio.carmo@gmail.com>
2022-03-29 14:48:04 -0300

    Initial commit

---
+++ src/main/java/br/com/sanziocarmo/aplicandotdd/AplicandotddApplication.java
@@ -0,0 +1,13 @@
+package br.com.sanziocarmo.aplicandotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicandotddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicandotddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sanziocarmo/aplicandotdd/AplicandotddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sanziocarmo.aplicandotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicandotddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
